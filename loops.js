ae646ef58547702b27925ea0f1006af102b64648
SHAIK IDRUS BASHA <shaikidrusbasha2001@gmail.com>
2024-10-24 00:31:53 +0530

    loops files are added

---
+++ loops.js
@@ -0,0 +1,36 @@
+// 1. printing numbers from 1 to 10
+
+for(var a=1;a<=10;a++){
+    console.log(a)
+}
+
+var a=1;
+while(a<=10){
+    console.log(a)
+    a++
+}
+
+    var a=1;
+    do{
+        console.log(a)
+    }while(a<=10)
+
+// 1. printing reverse  numbers from 1 to 10
+
+for(var a=6;a>=1;a--)
+{
+    console.log(a)
+}
+
+var a=6;
+while(a>=1)
+{
+  console.log(a)
+  a--
+}
+
+var a=6;
+do{
+    console.log(a)
+    a--;
+}while(a>=1)
